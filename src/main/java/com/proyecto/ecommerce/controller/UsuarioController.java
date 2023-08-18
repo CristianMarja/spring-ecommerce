9e994fe71c23c7c475f3137882b76fdead42a855
Cristian Martinez <Cristianmarja@gmail.com>
2023-08-17 21:03:22 -0500

    Obtener usuario login y sesion

--- src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
@@ -2,9 +2,14 @@ package com.proyecto.ecommerce.controller;
 
 
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +38,33 @@ public class UsuarioController {
 		logger.info("Usuario registro {}",usuario);
 		usuario.setTipo("USER");
 		usuarioService.save(usuario);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "usuario/login";
+	}
+	
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		logger.info("Accesos: {}",usuario);
+		
+		Optional<Usuario> user=usuarioService.findByEmail(usuario.getEmail());
+		//logger.info("Usuario de db: {}",user.get());
+		
+		if(user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if(user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";
+			}else {
+				return "redirect:/";
+			}
+		}else {
+			logger.info("Usuario no existe");
+		}
+		
 		return "redirect:/";
 	}
 }
