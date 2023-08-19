41963adfac98dd4175228928610803950fcef0c5
Cristian Martinez <Cristianmarja@gmail.com>
2023-08-19 00:24:12 -0500

    Mostrar la vista compras para el user

--- src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,5 +66,11 @@ public class UsuarioController {
 		}
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compras")
+	public String obtenerCompras(Model model,HttpSession session) {
+		model.addAttribute("sesion",session.getAttribute("idusuario"));
+		return "usuario/compras";
 	}
 }
